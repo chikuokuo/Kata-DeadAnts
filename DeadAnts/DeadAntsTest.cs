f4deb5c2c7b584404854c2ebc1e69fb7a1274208
Bell Chen Chi Kuo <chikuokuo@msn.com>
2017-09-11 22:33:45 +0800

    add first test case

---
+++ DeadAnts/DeadAntsTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DeadAnts
+{
+    [TestClass]
+    public class DeadAntsTest
+    {
+        [TestMethod]
+        public void String_Empty_ShouldReturn_Zero()
+        {
+            StringAverageShouldEqual(string.Empty, 0);
+        }
+
+        private static void StringAverageShouldEqual(string input, int expected)
+        {
+            DeadAnts deadAnts = new DeadAnts();
+            var result = deadAnts.DeadAntsCalculate(input);
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+
+    public class DeadAnts
+    {
+        public int DeadAntsCalculate(string antsTeam)
+        {
+            return 0;
+        }
+    }
+}
